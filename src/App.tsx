9f4b37b155fb84ea5d32d65f416f1512c362fd6a
georgkiim <kimkim99975@gmail.com>
2022-11-13 14:29:57 +0300

    create new social

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Content from "./components/MainContent";
+
 
 const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+      <div className='app-wrapper'>
+      <Header />
+        <Nav />
+        <Content />
+
+      </div>
+  )
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header =() => {
+  return (
+        <header className='header'>
+            <img src='https://w7.pngwing.com/pngs/941/692/png-transparent-black-small-apple-logo-logo-material-apple-logo-black.png'/>
+        </header>
+
+      )
+}
+
+export default Header
+
